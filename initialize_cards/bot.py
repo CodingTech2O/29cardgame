c9a84bdee807ce144ec4005e9163975bf7478320
CodingTech2O <asharanchess@gmail.com>
2026-09-13 23:35:05 +0530

    More Helper funcs made

--- initialize_cards/bot.py
+++ initialize_cards/bot.py
@@ -30,14 +30,29 @@ class Bot:
             if card in self.cards:
                 return True 
         return False
-    
+    def pair_in_card(cards):
+        king, queen = False, False
+        for card in cards:
+            if card.name.lower() == "king":
+                king = True
+            if card.name.lower() == "queen":
+                queen = True
+        return king and queen
+
+    def filter(self,cards,maxi=100,mini=0):
+        temp_cards =[]
+        for card in cards:
+            if card <maxi and mini < card:
+                temp_cards.append(card)
+        
 
     def decide_card_to_play(self,current_hand:list=None):
         if len(current_hand) == 0:
             if self.made_trump:
                 if self.check_if_card_in_cards("Jack",self.trump):
                     return Card("Jack",self.trump)
-                if self.check_any_card_in_cards(["7","8","K","Q"])
+#                if self.check_any_card_in_cards(["7","8","K","Q"]) and not self.pair_in_card(self.cards) and self.check_if_card_in_cards("9",self.trump):
+                    
                 
     def play_card(self,card):
         for i in range(len(self.cards)):
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from initialize_cards import initialize_cards,take_input_from_user,display_output_to_user
 from game import game
 
+
 bots,player = initialize_cards()
 game.state = "Playing"
 
-while len(game.played_hands) < 32:
-    
+bots[0]
